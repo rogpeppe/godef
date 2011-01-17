f807564af09e5cbde1e237625ab1b67713732555
Roger.Peppe <devnull@localhost>
2011-01-17 20:34:09 +0000

    typo

--- key/key.go
+++ key/key.go
@@ -3,7 +3,7 @@ package key
 // Mapping holds a set of unique keys corresponding
 // to Hasher values.
 type Mapping struct {
-	keys map[uint64] *entry
+	keys map[uint64]*entry
 }
 
 // Hasher represents a value that can be used as a map key.
@@ -14,7 +14,7 @@ type Hasher interface {
 
 type entry struct {
 	mkey Hasher
-	key Key
+	key  Key
 	next *entry
 }
 
@@ -27,7 +27,7 @@ type customKey struct {
 
 // NewMapping creates a new Mapping object
 func NewMapping() *Mapping {
-	return &Mapping{make(map[uint64] *entry)}
+	return &Mapping{make(map[uint64]*entry)}
 }
 
 // Key returns a comparable Key value corresponding
@@ -47,7 +47,7 @@ func (m *Mapping) Key(mkey Hasher) Key {
 	return k
 }
 
-// Value returns the original key value for a given Key,
+// Original returns the original key value for a given Key,
 // if it was created with the Key function;
 // otherwise it returns nil.
 func (m *Mapping) Original(k Key) Hasher {
