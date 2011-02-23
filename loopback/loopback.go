c0547dd27e0192a196576c6efaa44c0a3297c5ed
Roger.Peppe <devnull@localhost>
2011-02-23 18:03:55 +0000

    added loopback package

---
+++ loopback/loopback.go
@@ -0,0 +1,393 @@
+package loopback
+
+import (
+	"sync"
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+type block struct {
+	t int64
+	data []byte
+	prev *block
+	next *block
+	id int
+}
+
+type streamReader stream
+type streamWriter stream
+
+func (r *streamReader) Read(data []byte) (int, os.Error) {
+	return (*stream)(r).Read(data)
+}
+
+func (r *streamReader) Close() os.Error {
+	return (*stream)(r).closeInput()
+}
+
+func (w *streamWriter) Write(data []byte) (int, os.Error) {
+	return (*stream)(w).Write(data)
+}
+
+func (w *streamWriter) Close() os.Error {
+	return (*stream)(w).closeOutput()
+}
+
+type stream struct {
+	mu sync.Mutex
+
+	outClosed bool
+	inClosed bool
+
+	outTail *block			// sentinel.
+	outHead *block		// also transitTail.
+	transitHead *block		// also inTail.
+	inHead *block			// overall head of list.
+
+	outLimit int			// total size of output queue.
+	outAvail int			// free bytes in output queue.
+
+	inLimit int				// total size of input queue.
+	inAvail int				// free bytes in input queue.
+
+	byteDelay int64
+	latency int64
+	mtu int
+
+	notEmpty sync.Cond
+	notFull sync.Cond
+	waitNotEmpty int
+	waitNotFull int
+}
+
+type Options struct {
+	ByteDelay int64
+	Latency int64
+	InLimit int
+	OutLimit int
+	MTU int
+}
+
+func Pipe(opt Options) (io.ReadCloser, io.WriteCloser) {
+	if opt.MTU == 0 {
+		opt.MTU = 8192
+	}
+	if opt.InLimit == 0 {
+		opt.InLimit = 10 * opt.MTU
+	}
+	if opt.OutLimit == 0 {
+		opt.OutLimit = 10 * opt.MTU
+	}
+	if opt.InLimit < opt.MTU {
+		opt.InLimit = opt.MTU
+	}
+	if opt.OutLimit < opt.MTU {
+		opt.OutLimit = opt.MTU
+	}
+	sentinel := &block{id: -1}
+	s := &stream{
+		outLimit: opt.OutLimit,
+		outAvail: opt.OutLimit,
+		inLimit: opt.InLimit,
+		inAvail: opt.InLimit,
+		mtu: opt.MTU,
+		byteDelay: opt.ByteDelay,
+		latency: opt.Latency,
+		outTail: sentinel,
+		outHead: sentinel,
+		transitHead: sentinel,
+		inHead: sentinel,
+	}
+	s.notEmpty.L = &s.mu
+	s.notFull.L = &s.mu
+	return (*streamReader)(s), (*streamWriter)(s)
+}
+
+// Dodgy heuristic:
+// If there's stuff in the transit queue that's ready to
+// enter the input queue, but the input queue is full
+// and it's been waiting for at least latency ns,
+// then we block the output queue.
+// TODO what do we do about latency for
+// blocked packets - as it is a blocked packet
+// will incur less latency.
+func (s *stream) outBlocked(now int64) bool {
+	return s.transitHead != s.outHead &&
+		now >= s.transitHead.t + s.latency &&
+		s.inAvail < len(s.transitHead.data)
+}
+
+func (s *stream) closeInput() os.Error {
+	s.mu.Lock()
+	s.inClosed = true
+	if s.waitNotEmpty > 0 {
+		s.notEmpty.Broadcast()
+	}
+	if s.waitNotFull > 0 {
+		s.notFull.Broadcast()
+	}
+	s.mu.Unlock()
+	return nil
+}
+
+func (s *stream) closeOutput() os.Error {
+	s.mu.Lock()
+	s.outClosed = true
+	if s.waitNotEmpty > 0 {
+		s.notEmpty.Broadcast()
+	}
+	if s.waitNotFull > 0 {
+		s.notFull.Broadcast()
+	}
+	s.mu.Unlock()
+	return nil
+}
+
+func (s *stream) pushLink(now int64) {
+	doneSomething := false
+	if !s.outBlocked(now) {
+		// move blocks from out queue to transit queue.
+		for s.outTail != s.outHead && now >= s.outHead.t {
+			doneSomething = true
+			s.outHead.t += s.latency
+			s.outAvail += len(s.outHead.data)
+			s.outHead = s.outHead.next
+		}
+	}
+	// move blocks from transit queue to input queue
+	for s.transitHead != s.outHead && now >= s.transitHead.t {
+		if s.inAvail < len(s.transitHead.data) {
+			break		// or discard packet
+		}
+		doneSomething = true
+		s.inAvail -= len(s.transitHead.data)
+		s.transitHead = s.transitHead.next
+	}
+	if !doneSomething {
+//		fmt.Printf("pushLink did nothing\n")
+	}else{
+//		fmt.Printf("after pushLink:\n")
+//		fmt.Printf("\toutAvail %d; inAvail %d\n", s.outAvail, s.inAvail)
+//		fmt.Printf("\toutTail %v; outHead %v; transitHead %v; inHead %v\n", s.outTail, s.outHead, s.transitHead, s.inHead)
+	}
+}
+
+func (s *stream) Write(data []byte) (int, os.Error) {
+	// split the packet into MTU-sized portions if necessary.
+	for len(data) > s.mtu {
+		_, err := s.Write(data[0:s.mtu])
+		if err != nil {
+			return 0, err
+		}
+		data = data[s.mtu:]
+	}
+	s.mu.Lock()
+	now := time.Nanoseconds()
+	for {
+		s.pushLink(now)
+		if s.outAvail >= len(data) || s.outClosed{
+			break
+		}
+		if s.outBlocked(time.Nanoseconds()) {
+			if s.inClosed {
+				s.mu.Unlock()
+				return 0, os.EPIPE
+			}
+			s.waitNotFull++
+			s.notFull.Wait()
+			s.waitNotFull--
+			continue
+		}
+		t := s.earliestWriteTime(len(data))
+		now = s.sleepUntil(t)
+	}
+	if s.outClosed {
+		s.mu.Unlock()
+		return 0, os.EPIPE
+	}
+	delay := int64(len(data)) * s.byteDelay
+	var t int64
+	// If there's a block in the queue that's not yet due
+	// for transit, then this block leaves delay ns after
+	// that one.
+	if s.outHead != s.outTail && now < s.outTail.prev.t {
+		t = s.outTail.prev.t + delay
+	}else{
+		t = now + delay
+	}
+	s.addBlock(t, s.copy(data))
+	s.outAvail -= len(data)
+
+	if s.waitNotEmpty > 0 {
+		s.notEmpty.Broadcast()
+	}
+	s.mu.Unlock()
+	// TODO runtime.Gosched() ?
+	return len(data), nil
+}
+
+func (s *stream) Read(buf []byte) (int, os.Error) {
+	s.mu.Lock()
+	// Loop until there's something to read from the input queue.
+	now := time.Nanoseconds()
+	for {
+		s.pushLink(now)
+		if s.inHead != s.transitHead {
+			break
+		}
+		if s.inHead == s.outTail {
+			// No data at all in the queue.
+			// If the queue is empty and the output queue is closed,
+			// then we see EOF.
+			if s.outClosed {
+				s.mu.Unlock()
+				return 0, os.EOF
+			}
+			s.waitNotEmpty++
+			s.notEmpty.Wait()
+			s.waitNotEmpty--
+			continue
+		}
+		now = s.sleepUntil(s.earliestReadTime())
+	}
+	if s.inClosed {
+		// input queue has been forcibly closed:
+		// TODO is os.EOF the right error here?
+		return 0, os.EOF
+	}
+	b := s.inHead
+	n := copy(buf, b.data)
+	b.data = b.data[n:]
+	s.inAvail += n
+	if len(b.data) == 0 {
+		s.removeBlock()
+	}
+	// Wake up any writers blocked on a full queue.
+	if s.waitNotFull > 0 {
+		s.notFull.Broadcast()
+	}
+	s.mu.Unlock()
+	return n, nil
+}
+
+// earliestReadTime returns the earliest time that
+// some data might arrive into the input queue.
+// It assumes that there is some data in the system.
+func (s *stream) earliestReadTime() int64 {
+	if s.inAvail < s.inLimit {
+		// data is available right now.
+		return 0
+	}
+	if s.transitHead != s.outHead {
+		return s.transitHead.t
+	}
+	if s.outHead != s.outTail {
+		return s.outHead.t + s.latency
+	}
+	panic("no data")
+}
+
+// earliestWriteTime returns the earliest time that
+// there may be space for n bytes of data to be
+// placed into the output queue (it might be later
+// if packets are dropped).
+func (s *stream) earliestWriteTime(n int) int64 {
+	if s.outAvail < s.outLimit {
+		// space is available now.
+		return 0
+	}
+	tot := s.outAvail
+	for b := s.outHead; b != s.outTail; b = b.next {
+		tot += len(b.data)
+		if tot >= n {
+			return b.t
+		}
+	}
+	panic("write limit exceeded by block size")
+}
+
+// sleep until the absolute time t.
+// Called with lock held.
+func (s *stream) sleepUntil(t int64) int64 {
+	now := time.Nanoseconds()
+	if now >= t {
+		return now
+	}
+	s.mu.Unlock()
+	time.Sleep(t - now)
+	s.mu.Lock()
+	return time.Nanoseconds()
+}
+
+func (s *stream) copy(x []byte) []byte {
+	y := make([]byte, len(x))
+	copy(y, x)
+	return y
+}
+
+// addBlock adds a block to the head of the queue.
+// It does not adjust queue stats.
+func (s *stream) addBlock(t int64, data []byte) {
+	// If there are no items in output queue, replace sentinel block
+	// so that other pointers into queue do not need
+	// to change.
+	if s.outHead == s.outTail {
+		s.outHead.t = t
+		s.outHead.data = data
+		s.outHead.id = newid()
+		s.outHead.next = &block{prev: s.outHead, id: -1}	// new sentinel
+		s.outTail = s.outHead.next
+		return
+	}
+
+	// Add a new block just after the sentinel.	
+	b := &block{
+		t: t, 
+		data: data,
+		id: newid(),
+	}
+	b.next = s.outTail
+	b.prev = s.outTail.prev
+
+	s.outTail.prev = b
+	b.prev.next = b
+}
+
+// Remove the block from the front of the queue.
+// (assumes that there is such a block to remove)
+func (s *stream) removeBlock() {
+	b := s.inHead
+	s.inHead = b.next
+	if s.inHead != nil {
+		s.inHead.prev = nil
+	}
+	// help garbage collector
+	b.next = nil
+	b.prev = nil
+}
+
+var maxid int
+func newid() (id int) {
+	id = maxid
+	maxid++
+	return
+}
+
+func (s *stream) print(msg string) {
+	return
+	fmt.Printf("%s\n", msg)
+	fmt.Printf("\toutAvail %d; inAvail %d\n", s.outAvail, s.inAvail)
+	fmt.Printf("\toutTail %v; outHead %v; transitHead %v; inHead %v\n", s.outTail, s.outHead, s.transitHead, s.inHead)
+	for b := s.outTail; b != nil; b = b.prev {
+		fmt.Printf("\t%v; prev %v; next %v; len %d\n", b, b.prev, b.next, len(b.data))
+	}
+}
+
+func (b *block) String() string {
+	if b == nil {
+		return "nil"
+	}
+	return fmt.Sprint("b", b.id)
+}
---
+++ loopback/loopback_test.go
@@ -0,0 +1,242 @@
+package loopback
+import (
+	"encoding/binary"
+	"io"
+	"os"
+	"testing"
+	"time"
+)
+
+func TestSimpleWriteRead(t *testing.T) {
+	r, w := Pipe(Options{})
+	msg := []byte("hello, world")
+	n, err := w.Write(msg)
+	if err != nil {
+		t.Fatalf("write error: %v", err)
+	}
+	if n != len(msg) {
+		t.Fatalf("write of too few bytes, expected %d, got %d", len(msg), n)
+	}
+	buf := make([]byte, 100)
+	n, err = r.Read(buf)
+	if err != nil {
+		t.Fatalf("read error: %v", err)
+	}
+	if n != len(msg) {
+		t.Fatalf("too few bytes")
+	}
+	buf = buf[0:n]
+	if string(buf) != string(msg) {
+		t.Fatalf("received wrong data: %q", buf)
+	}
+}
+
+func TestOutputClose(t *testing.T) {
+	r, w := Pipe(Options{})
+	writePacket(t, w, make([]byte, 14), 0)
+	w.Close()
+
+	buf := make([]byte, 14)
+	readPacket(t, r, buf, 0)
+	_, err := r.Read(buf)
+	if err != os.EOF {
+		t.Fatalf("expected os.EOF, got %v", err)
+	}
+}
+
+func TestInputClose(t *testing.T) {
+	r, w := Pipe(Options{MTU: 100, InLimit: 2*100, OutLimit: 2*100})
+	sync := make(chan bool)
+	go func() {
+		buf := make([]byte, 100)
+		for i := 0; i < 10; i++ {
+			_, err := w.Write(buf)
+			if err != nil {
+				if err != os.EPIPE {
+					t.Fatalf("expected EPIPE error; got %v", err)
+				}
+				break
+			}
+		}
+		sync <- true
+	}()
+	r.Close()
+	select{
+	case <-time.After(0.2e9):
+		t.Fatalf("close did not wake up writer")
+	case <-sync:
+	}
+}
+
+func TestLatency(t *testing.T) {
+	const (
+		n = 10
+		latency = int64(0.1e9)
+		leeway = int64(0.01e9)
+	)
+	r, w := Pipe(Options{Latency: 0.1e9})
+	go writeNValues(t, w, n, make([]byte, 14), 0.1e9)
+	buf := make([]byte, 14)
+	for i := 0; i < 10; i++ {
+		now, sentTime := readPacket(t, r, buf, i)
+		if abs(now - sentTime - latency) > leeway {
+			t.Errorf("expected latency of %dns; got %dns\n", latency, now - sentTime)
+		}
+	}
+}
+
+func TestBandwidth(t *testing.T) {
+	const (
+		n = 10
+		packetSize = 8192
+		bandwidth = int64((1024*1024)/8)	// 1 Mbit in bytes
+		delay = 1e9 / bandwidth		// byte delay in ns.
+	)
+	r, w := Pipe(Options{ByteDelay: delay, MTU: 8192})
+	go writeNValues(t, w, n, make([]byte, 8192), 0)
+	buf := make([]byte, 8192)
+	var t0, t1 int64
+	for i := 0; i < n; i++ {
+		now, sent := readPacket(t, r, buf, i)
+		t1 = now
+		if t0 == 0 {
+			t0 = sent
+		}
+	}
+	expect := delay * int64(packetSize * n)
+	got := t1 - t0
+	if abs(expect - got) * 100 / expect > 1 {
+		t.Error("wrong bandwidth; expected %dns; got %dns", expect, got)
+	}
+}
+
+func TestWriteBlocking(t *testing.T){
+	r, w := Pipe(Options{MTU: 14, InLimit: 14*2, OutLimit: 14*2})
+	sync := make(chan bool)
+	go func(){
+		// fill buffers - 2 for the buffer at each end, and
+		// one blocked in transit.
+		writeNValues(t, w, 5, make([]byte, 14), 0)
+		sync <- true
+		// write one more, which should block.
+		writePacket(t, w, make([]byte, 14), 99)
+		sync <- true
+	}()
+	// Check that write has not blocked filling the buffers.
+	select{
+	case <-time.After(0.2e9):
+		t.Fatalf("writer blocked too early")
+	case <-sync:
+	}
+
+	time.Sleep(0.2e9)
+
+	// Check that write has correctly blocked.
+	select{
+	case <-sync:
+		t.Fatalf("writer did not block")
+	default:
+	}
+
+	// check that write unblocks when we read a packet.
+	readPacket(t, r, make([]byte, 14), 0)
+	time.Sleep(0.2e9)
+	select{
+	case <-time.After(0.2e9):
+		t.Fatalf("writer still blocked")
+	case <-sync:
+	}
+}
+
+func BenchmarkPacketTransfer(b *testing.B) {
+	r, w := Pipe(Options{})
+	bufSize := 100
+	b.SetBytes(int64(bufSize))
+	go func() {
+		buf := make([]byte, bufSize)
+		for i := b.N-1; i >= 0; i-- {
+			w.Write(buf)
+		}
+	}()
+	buf := make([]byte, bufSize)
+	for i := b.N-1; i >= 0; i-- {
+		n, err := r.Read(buf)
+		if n != bufSize || err != nil {
+			panic("read failed")
+		}
+	}
+}
+
+func BenchmarkPipeTransfer(b *testing.B) {
+	r, w := io.Pipe()
+	bufSize := 100
+	b.SetBytes(int64(bufSize))
+	go func() {
+		buf := make([]byte, bufSize)
+		for i := b.N-1; i >= 0; i-- {
+			w.Write(buf)
+		}
+	}()
+	buf := make([]byte, bufSize)
+	for i := b.N-1; i >= 0; i-- {
+		n, err := r.Read(buf)
+		if n != bufSize || err != nil {
+			panic("read failed")
+		}
+	}
+}
+
+const check = 0xfea1
+
+func writeNValues(t *testing.T, s io.Writer, n int, buf []byte, period int64) {
+	for i := 0; i < n; i++ {
+		writePacket(t, s, buf, i)
+		if period > 0 {
+			time.Sleep(period)
+		}
+	}
+}
+
+func writePacket(t *testing.T, s io.Writer, buf []byte, index int){
+	if len(buf) < 14 {
+		panic("buf too small for header")
+	}
+	binary.LittleEndian.PutUint16(buf, check)
+	binary.LittleEndian.PutUint64(buf[2:], uint64(time.Nanoseconds()))
+	binary.LittleEndian.PutUint32(buf[10:], uint32(index))
+	n, err := s.Write(buf)
+	if err != nil {
+		t.Fatalf("write error: %v", err)
+	}
+	if n != len(buf) {
+		t.Fatalf("write count: expected %d; got %d", len(buf), n)
+	}
+}
+
+func readPacket(t *testing.T, s io.Reader, buf []byte, index int) (int64, int64) {
+	n, err := s.Read(buf)
+	now := time.Nanoseconds()
+	if err != nil {
+		t.Fatalf("read error: %v", err)
+	}
+	if n != len(buf) {
+		t.Fatalf("read count: expected %d; got %d", len(buf), n)
+	}
+	c := int(binary.LittleEndian.Uint16(buf))
+	if c != check {
+		t.Fatalf("invalid check; expected %#x; got %#x", check, c)
+	}
+	sentIndex := int(binary.LittleEndian.Uint32(buf[10:14]))
+	if sentIndex != index {
+		t.Errorf("block arrived out of order; expected %d; got %d", index, sentIndex)
+	}
+	sentTime := int64(binary.LittleEndian.Uint64(buf[2:10]))
+	return now, sentTime
+}
+
+func abs(x int64) int64 {
+	if x >= 0 {
+		return x
+	}
+	return -x
+}
