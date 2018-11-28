8a94ed6ddf486226cc2f4278d1651b3677e6a7c0
Ian Cottrell <iancottrell@google.com>
2018-11-27 20:14:42 -0500

    add tests for the printing part of godef

--- godef_test.go
+++ godef_test.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"go/build"
 	"go/token"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 	"testing"
 
@@ -50,40 +53,13 @@ func runGoDefTest(t testing.TB, exporter packagestest.Exporter, runCount int, mo
 	}
 
 	count := 0
-	exported.Expect(map[string]interface{}{
+	if err := exported.Expect(map[string]interface{}{
 		"godef": func(src, target token.Position) {
 			count++
-			input, err := ioutil.ReadFile(src.Filename)
+			obj, _, err := invokeGodef(src, runCount)
 			if err != nil {
-				t.Errorf("Failed %v: %v", src, err)
+				t.Error(err)
 				return
-			}
-			// There's a "saved" version of the file, so
-			// copy it to the original version; we want the
-			// Expect method to see the in-editor-buffer
-			// versions of the files, but we want the godef
-			// function to see the files as they should
-			// be on disk, so that we're actually testing the
-			// define-in-buffer functionality.
-			savedFile := src.Filename + ".saved"
-			if _, err := os.Stat(savedFile); err == nil {
-				savedData, err := ioutil.ReadFile(savedFile)
-				if err != nil {
-					t.Fatalf("cannot read saved file: %v", err)
-				}
-				if err := ioutil.WriteFile(src.Filename, savedData, 0666); err != nil {
-					t.Fatalf("cannot write saved file: %v", err)
-				}
-				defer ioutil.WriteFile(src.Filename, input, 0666)
-			}
-			// repeat the actual godef part n times, for benchmark support
-			var obj *ast.Object
-			for i := 0; i < runCount; i++ {
-				obj, _, err = godef(src.Filename, input, src.Offset)
-				if err != nil {
-					t.Errorf("Failed %v: %v", src, err)
-					return
-				}
 			}
 			pos := types.FileSet.Position(types.DeclPos(obj))
 			check := token.Position{
@@ -96,13 +72,88 @@ func runGoDefTest(t testing.TB, exporter packagestest.Exporter, runCount int, mo
 				t.Errorf("Got %v expected %v", posStr(check), posStr(target))
 			}
 		},
-	})
+		"godefPrint": func(src token.Position, mode string, re *regexp.Regexp) {
+			count++
+			obj, typ, err := invokeGodef(src, runCount)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			buf := &bytes.Buffer{}
+			switch mode {
+			case "json":
+				*jsonFlag = true
+				*tflag = false
+				*aflag = false
+				*Aflag = false
+			case "all":
+				*jsonFlag = false
+				*tflag = true
+				*aflag = true
+				*Aflag = true
+			case "public":
+				*jsonFlag = false
+				*tflag = true
+				*aflag = true
+				*Aflag = false
+			case "type":
+				*jsonFlag = false
+				*tflag = true
+				*aflag = false
+				*Aflag = false
+			default:
+				t.Fatalf("Invalid print mode %v", mode)
+			}
+
+			print(buf, obj, typ)
+			if !re.Match(buf.Bytes()) {
+				t.Errorf("in mode %q got %v want %v", mode, buf, re)
+			}
+		},
+	}); err != nil {
+		t.Fatal(err)
+	}
 	if count == 0 {
 		t.Fatalf("No godef tests were run")
 	}
 }
 
 var cwd, _ = os.Getwd()
+
+func invokeGodef(src token.Position, runCount int) (*ast.Object, types.Type, error) {
+	input, err := ioutil.ReadFile(src.Filename)
+	if err != nil {
+		return nil, types.Type{}, fmt.Errorf("Failed %v: %v", src, err)
+	}
+	// There's a "saved" version of the file, so
+	// copy it to the original version; we want the
+	// Expect method to see the in-editor-buffer
+	// versions of the files, but we want the godef
+	// function to see the files as they should
+	// be on disk, so that we're actually testing the
+	// define-in-buffer functionality.
+	savedFile := src.Filename + ".saved"
+	if _, err := os.Stat(savedFile); err == nil {
+		savedData, err := ioutil.ReadFile(savedFile)
+		if err != nil {
+			return nil, types.Type{}, fmt.Errorf("cannot read saved file: %v", err)
+		}
+		if err := ioutil.WriteFile(src.Filename, savedData, 0666); err != nil {
+			return nil, types.Type{}, fmt.Errorf("cannot write saved file: %v", err)
+		}
+		defer ioutil.WriteFile(src.Filename, input, 0666)
+	}
+	// repeat the actual godef part n times, for benchmark support
+	var obj *ast.Object
+	var typ types.Type
+	for i := 0; i < runCount; i++ {
+		obj, typ, err = godef(src.Filename, input, src.Offset)
+		if err != nil {
+			return nil, types.Type{}, fmt.Errorf("Failed %v: %v", src, err)
+		}
+	}
+	return obj, typ, nil
+}
 
 func localPos(pos token.Position, e *packagestest.Exported, modules []packagestest.Module) string {
 	fstat, fstatErr := os.Stat(pos.Filename)
